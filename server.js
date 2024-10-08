6e73e37e6a8e766762d66780f39a5a61167bf543
gautham-pws <gautham.p@pacewisdom.com>
2024-10-08 11:19:34 +0530

    code formatted

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ const server = createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at https://${hostname}:${port}/`);
 });
