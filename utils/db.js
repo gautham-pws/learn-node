f4cd37a2539adb3bf0ea757d35e28f32392d989f
gautham-pws <gautham.p@pacewisdom.com>
2024-10-09 12:20:52 +0530

    crud operation

--- utils/db.js
+++ utils/db.js
@@ -16,21 +16,41 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+//    await client.db("admin").command({ ping: 1 });
 //    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    const db = client.db('test')
-    db.collection('users').insertOne({
-      name: 'gautham',
-      age: 28,
+//    const db = client.db('test')
+//    db.collection('users').insertOne({
+//      name: 'gautham',
+//      age: 28,
+//
+//    }).then(res => console.log(res)).catch(e => console.log(e))
 
-    }).then(res => console.log(res)).catch(e => console.log(e))
+//    await client.db('project0').collection('users').insertOne({
+//      name: 'gpg',dob: 96
+//    })
+//
+//    await client.db("project0").collection("users").insertMany([{name: "abc", dob: 45}, {value: 'cd',dobds:23}]).then(res=>console.log(res)).catch(e=>console.log(e))
 
-    await client.db('project0').collection('users').insertOne({
-      name: 'gpg',dob: 96
-    })
+//await client.db("project0").collection("users").findOne({}).then((res)=>{console.log(res)}).catch((e)=>console.log(e))
+//await client.db("project0").collection("users").find({dob: 45}).toArray().then((res)=>{console.log(res)}).then((e)=>console.log(e))
 
-    await client.db("project0").collection("users").insertMany([{name: "abc", dob: 45}, {value: 'cd',dobds:23}]).then(res=>console.log(res)).catch(e=>console.log(e))
-
+//await client.db("project0").collection("users").updateOne(
+//  {dob:45},{
+//    $set:{
+//      dob: 32
+//    }
+//  }
+//).then((res)=>console.log(res)).catch((e)=>{conso.e.log(e)})
+//await client.db("project0").collection("users").updateMany(
+//  {},{
+//    $inc:{
+//      dob: 11
+//    }
+//  }
+//).then((res)=>console.log(res)).catch((e)=>{console.log(e)})
+await client.db("project0").collection("users").deleteMany({dob: 56
+  }
+).then((res)=>console.log(res)).catch((e)=>{console.log(e)})
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
