60a29f89c210e60a056d64e52829bc74933b6eb3
gautham-pws <gautham.p@pacewisdom.com>
2024-10-09 17:54:50 +0530

    bcrypt

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const bcrypt = require("bcrypt")
+
 const uri = "mongodb+srv://gauthamp:Er4Dz26iRlHxI11c@cluster0.gjgpd.mongodb.net/project0?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(uri)
 
@@ -37,12 +39,28 @@ const userSchema = new Schema({
 })
 
 
-const User = mongoose.model('User', userSchema)
-
 //const user1 = new User({
 //	name: "gpg", age: 233, email: "gautham@gmail.com", password: "123"
 //})
 
 //user1.save().then((a) => console.log(a)).then((e) => console.log(e))
+
+//userSchema.pre('save', async function (next) {
+//	const user = this;
+//	if (user.isModified('password')) {
+//		user.password = await bcrypt.hash(user.password, 8)
+//	}
+//	next()
+//})
+
+
+userSchema.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		this.password = await bcrypt.hash(this.password, 8);
+	}
+	next();
+});
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
