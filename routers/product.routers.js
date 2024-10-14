398caf9a02043ccec73f335f6699b091f3e0ee1e
gautham-pws <gautham.p@pacewisdom.com>
2024-10-14 16:07:45 +0530

    feat (routes): placeholder

---
+++ routers/product.routers.js
@@ -0,0 +1,23 @@
+// GET api/products get all products
+
+// GET api/products/:id get products by id
+
+// POST api/products add new products
+
+// PUT api/products/:id update products by id
+
+// DELETE api/products/:id remove products by id
+
+// DELETE api/products remove all products
+
+// GET api/products/published find all published products
+
+// GET api/products?name= find all products by name
+
+// GET api/products/:userId get all products by user id
+
+// DELETE api/products/:id remove products by id
+
+// POST api/products/:userid add new products by userid
+
+// PUT api/products/:userid and product id update products by userid
---
+++ routers/user.routers.js
@@ -0,0 +1,9 @@
+// POST api/users add new users
+
+// PUT api/users/:id update users by id
+
+// GET api/users get all users
+
+// GET api/users/:id get user details by id
+
+// DELETE api/users delete user by id
