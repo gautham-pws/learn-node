c27dc53607b2481c03119910fab4b932076fb3e4
gautham-pws <gautham.p@pacewisdom.com>
2024-10-21 14:20:33 +0530

    cron changed to 24 hours

--- middleware/scheduler.js
+++ middleware/scheduler.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const sendNotifications = require("./notification");
 
 // Schedule the task to run every 2 minutes
-cron.schedule("*/1 * * * *", () => {
+cron.schedule("0 * * * *", () => {
   console.log(`Running notification job at ${new Date().toISOString()}...`);
   sendNotifications();
 });
