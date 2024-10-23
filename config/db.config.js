246391304f3ec81c97b82f02ccba00209579c044
gautham-pws <gautham.p@pacewisdom.com>
2024-10-23 14:29:27 +0530

    uri moved to env file

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.MONGO_URI;
 
 mongoose
   .connect(uri)
