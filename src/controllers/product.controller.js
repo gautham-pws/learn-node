26c5953dc8358f917b9b417fd5ed5a0d086130bb
gautham-pws <gautham.p@pacewisdom.com>
2024-10-24 16:49:50 +0530

    feat(pagination): get products with filter, sort and pagination

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,8 +3,8 @@ import {Product} from "../models/index.js";
 import resFormat from "../utilities/resFormat.js";
 
 // get all the prodcuts by default,
-// additionally can pass any query parameter filtering and sort by specific order
-// example: http://localhost:3000/api/products?sortBy=rating:dsc&userId=6718e0ccae18fa7277624a6c
+// additionally can pass any query parameteres for filtering, sort by specific order and pagination with page and limit parameter
+// example: http://localhost:3000/api/products?userId=6718e0ccae18fa7277624a6c&sortBy=rating:dsc&limit=3&page=2
 export const getProducts = async (req, res) => {
   try {
     const query = {};
@@ -29,6 +29,8 @@ export const getProducts = async (req, res) => {
           query[key] = Number(value);
           break;
         case "sortBy":
+        case "limit":
+        case "page":
           break;
         default:
           query[key] = value;
@@ -52,13 +54,18 @@ export const getProducts = async (req, res) => {
       });
     }
 
-    const products = await Product.find(query).sort(sortBy);
-    console.log("🚀 ~ getProducts ~ query:", query);
-    console.log("🚀 ~ getProducts ~ sortBy:", sortBy);
+    // Pagination parameters
+    const page = Math.max(parseInt(req.query.page) || 1, 1); // Ensure page is at least 1
+    const limit = Math.max(parseInt(req.query.limit) || 10, 1); // Ensure limit is at least 1
+    const skip = (page - 1) * limit; // Calculate skip value for pagination
+    const products = await Product.find(query)
+      .sort(sortBy)
+      .skip(skip)
+      .limit(limit);
 
     // Remove the image field from each product
     const sanitizedProducts = products.map((product) => {
-      const productObject = product.toObject(); // Convert Mongoose document to plain JavaScript object
+      const productObject = product.toObject();
       delete productObject.image; // Remove the image field
       return productObject;
     });
@@ -87,7 +94,6 @@ export const getProducts = async (req, res) => {
 export const createProduct = async (req, res) => {
   try {
     const productData = req.body;
-    console.log("🚀 ~ createProduct ~ productData:", productData);
     if (req.file) {
       productData.image = req.file.buffer;
     }
