c9b627f8d62a6c96289a16751dfdf8807089ef29
gautham-pws <gautham.p@pacewisdom.com>
2024-11-05 14:55:42 +0530

    removed all the code

--- src/models/index.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import "../services/user.service.js";
-import "../services/product.service.js";
-import productSchema from "./product.schema.js";
-import userSchema from "./user.schema.js";
-
-export const Product = mongoose.model("Product", productSchema);
-export const User = mongoose.model("User", userSchema);
